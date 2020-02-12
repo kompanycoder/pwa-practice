0ca1df68ef1d9e1d3d92aa768ca9a0b6eda5f72e
kompanycoder <mwegagideon@gmail.com>
2020-02-12 14:09:05 +0300

    completed service workers and pwa install on home screen

--- offline-quickstart-lab/app/service-worker.js
+++ offline-quickstart-lab/app/service-worker.js
@@ -1,1 +1,43 @@
+let self = this;
 
+// cache name
+let cacheName = 'cache-v1';
+// resources to cache
+let siteResources = [
+    '/',
+    'index.html',
+    'styles/main.css',
+    'images/space1.jpg',
+    'images/space2.jpg',
+    'images/space3.jpg'
+];
+
+// install service worker
+self.addEventListener('install', (event)=>{
+    console.log('in the event install. :-) ', event);
+    event.waitUntil(
+        caches.open(cacheName).then((cacheObj)=>{
+           return cacheObj.addAll(siteResources);
+        }).catch((err)=>{
+            console.log(err);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) =>{
+    console.log('Activate event triggered!');
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log('fetch event triggered for ', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse)=>{
+            if(cachedResponse){
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        }).catch((err)=>{
+            console.log('Oops.. found an error' + err);
+        })
+    )
+});
